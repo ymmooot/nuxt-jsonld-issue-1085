ecd1583b9d68c479ea531c8aee7e716b73ef022c
ryota yamamoto <ryota.yama.9+github@gmail.com>
2023-02-05 02:52:09 +0900

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <Tutorial />
+</template>
+
+<script>
+export default {
+  jsonld() {
+    return {
+      "@context": "https://schema.org",
+      "@type": "Product",
+      name: "product name",
+    };
+  },
+};
+</script>
